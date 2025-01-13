e04c2d10e59b5e50d8ffafceddda194d61364ed7
Leonardo Djekki <leo.djekki@gmail.com>
2025-01-12 21:53:28 -0600

    First commit inicial creation of TextInput component - TextInput.jsx

---
+++ src/components/TextInput.jsx
@@ -0,0 +1,7 @@
+// UI requirements:
+// AddIcon on the left
+// Install MUI icons library - npm install @mui/icons-material
+// TextInput on the right
+// Event should be fired when the user hit the return key
+// The text should be cleared when the user hits the return key
+// Test with Storybook
