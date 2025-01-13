7b39e5da1afc772065e1ead441688a556a455d81
Leonardo Djekki <leo.djekki@gmail.com>
2025-01-12 22:57:36 -0600

    material ui icons installed mui/@mui/icons-material, and create the basic of TextInput using mui components API

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -5,3 +5,30 @@
 // Event should be fired when the user hit the return key
 // The text should be cleared when the user hits the return key
 // Test with Storybook
+
+// using Default Imports
+import React from 'react';
+import Paper  from '@mui/material/Paper'; 
+import InputBase  from '@mui/material/InputBase';
+
+// import {InputBase} from '@mui/material' // Named import
+//import {AddIcon }  from '@mui/icons-material'; // Named import
+import  AddIcon  from '@mui/icons-material/Add'; // Default import - faster load time
+
+export const TextInput = ({placeholder, onChange, onSubmit, value }) => {
+    // paper
+    // AddIcon
+    // InputBase
+    return (
+        <Paper
+         onSubmit={onSubmit}
+        >
+            <AddIcon />
+            <InputBase 
+                placeholder={placeholder}
+                onChange={onChange}
+                value={value}
+            />
+        </Paper>
+    );
+}
